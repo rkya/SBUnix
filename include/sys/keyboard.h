65f999b51227052645198fa5faeb4cb824e294cf
rkya <rkarhadkar@cs.stonybrook.edu>
2018-04-29 22:54:55 -0400

    Hash defined keyboard constants

--- include/sys/keyboard.h
+++ include/sys/keyboard.h
@@ -6,116 +6,115 @@
 #include <sys/io.h>
 
 /*keyboard scan codes referred from https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html*/
-enum scan_codes {
-  KB_ERR = 0x00,
-  KB_ESC = 0x01,
 
-  KB_1 = 0x02,
-  KB_2 = 0x03,
-  KB_3 = 0x04,
-  KB_4 = 0x05,
-  KB_5 = 0x06,
-  KB_6 = 0x07,
-  KB_7 = 0x08,
-  KB_8 = 0x09,
-  KB_9 = 0x0a,
-  KB_0 = 0x0b,
+#define KEYBOARD_ERR 0x00
+#define KEYBOARD_ESC 0x01
 
-  KB_MINUS_UNDERSCORE = 0x0c,
-  KB_EQUAL_PLUS = 0x0d,
-  KB_BACKSPACE = 0x0e,
+#define KEYBOARD_1 0x02
+#define KEYBOARD_2 0x03
+#define KEYBOARD_3 0x04
+#define KEYBOARD_4 0x05
+#define KEYBOARD_5 0x06
+#define KEYBOARD_6 0x07
+#define KEYBOARD_7 0x08
+#define KEYBOARD_8 0x09
+#define KEYBOARD_9 0x0a
+#define KEYBOARD_0 0x0b
 
-  KB_TAB = 0x0f,
-  KB_Q = 0x10,
-  KB_W = 0x11,
-  KB_E = 0x12,
-  KB_R = 0x13,
-  KB_T = 0x14,
-  KB_Y = 0x15,
-  KB_U = 0x16,
-  KB_I = 0x17,
-  KB_O = 0x18,
-  KB_P = 0x19,
-  KB_BRACES_OPEN = 0x1a,
-  KB_BRACES_CLOSE = 0x1b,
+#define KEYBOARD_MINUS_UNDERSCORE 0x0c
+#define KEYBOARD_EQUAL_PLUS 0x0d
+#define KEYBOARD_BACKSPACE 0x0e
 
-  KB_ENTER = 0x1c,
+#define KEYBOARD_TAB 0x0f
+#define KEYBOARD_Q 0x10
+#define KEYBOARD_W 0x11
+#define KEYBOARD_E 0x12
+#define KEYBOARD_R 0x13
+#define KEYBOARD_T 0x14
+#define KEYBOARD_Y 0x15
+#define KEYBOARD_U 0x16
+#define KEYBOARD_I 0x17
+#define KEYBOARD_O 0x18
+#define KEYBOARD_P 0x19
+#define KEYBOARD_BRACES_OPEN 0x1a
+#define KEYBOARD_BRACES_CLOSE 0x1b
 
-  KB_L_CONTROL = 0x1d,
+#define KEYBOARD_ENTER 0x1c
 
-  KB_A = 0x1e,
-  KB_S = 0x1f,
-  KB_D = 0x20,
-  KB_F = 0x21,
-  KB_G = 0x22,
-  KB_H = 0x23,
-  KB_J = 0x24,
-  KB_K = 0x25,
-  KB_L = 0x26,
-  KB_SEMICOLON_COMMA = 0x27,
-  KB_INVERTED_COMMA = 0x28,
+#define KEYBOARD_L_CONTROL 0x1d
 
-  KB_TILDE = 0x29,
+#define KEYBOARD_A 0x1e
+#define KEYBOARD_S 0x1f
+#define KEYBOARD_D 0x20
+#define KEYBOARD_F 0x21
+#define KEYBOARD_G 0x22
+#define KEYBOARD_H 0x23
+#define KEYBOARD_J 0x24
+#define KEYBOARD_K 0x25
+#define KEYBOARD_L 0x26
+#define KEYBOARD_SEMICOLON_COMMA 0x27
+#define KEYBOARD_INVERTED_COMMA 0x28
 
-  KB_L_SHIFT = 0x2a,
+#define KEYBOARD_TILDE 0x29
 
-  KB_B_SLASH = 0x2b,
+#define KEYBOARD_L_SHIFT 0x2a
 
-  KB_Z = 0x2c,
-  KB_X = 0x2d,
-  KB_C = 0x2e,
-  KB_V = 0x2f,
-  KB_B = 0x30,
-  KB_N = 0x31,
-  KB_M = 0x32,
-  KB_COMMA_ANGLE_OPEN = 0x33,
-  KB_DOT_ANGLE_CLOSE = 0x34,
-  KB_F_SLASH_QUESTION = 0x35,
-  KB_R_SHIFT = 0x36,
+#define KEYBOARD_B_SLASH 0x2b
 
-  KB_STAR = 0x37,
+#define KEYBOARD_Z 0x2c
+#define KEYBOARD_X 0x2d
+#define KEYBOARD_C 0x2e
+#define KEYBOARD_V 0x2f
+#define KEYBOARD_B 0x30
+#define KEYBOARD_N 0x31
+#define KEYBOARD_M 0x32
+#define KEYBOARD_COMMA_ANGLE_OPEN 0x33
+#define KEYBOARD_DOT_ANGLE_CLOSE 0x34
+#define KEYBOARD_F_SLASH_QUESTION 0x35
+#define KEYBOARD_R_SHIFT 0x36
 
-  KB_L_ALT = 0x38,
-  KB_SPACE = 0x39,
+#define KEYBOARD_STAR 0x37
 
-  KB_CAPS = 0x3a,
+#define KEYBOARD_L_ALT 0x38
+#define KEYBOARD_SPACE 0x39
 
-  KB_F1 = 0x3b,
-  KB_F2 = 0x3c,
-  KB_F3 = 0x3d,
-  KB_F4 = 0x3e,
-  KB_F5 = 0x3f,
-  KB_F6 = 0x40,
-  KB_F7 = 0x41,
-  KB_F8 = 0x42,
-  KB_F9 = 0x43,
-  KB_F10 = 0x44,
+#define KEYBOARD_CAPS 0x3a
 
-  KB_NUM_LOCK = 0x45,
+#define KEYBOARD_F1 0x3b
+#define KEYBOARD_F2 0x3c
+#define KEYBOARD_F3 0x3d
+#define KEYBOARD_F4 0x3e
+#define KEYBOARD_F5 0x3f
+#define KEYBOARD_F6 0x40
+#define KEYBOARD_F7 0x41
+#define KEYBOARD_F8 0x42
+#define KEYBOARD_F9 0x43
+#define KEYBOARD_F10 0x44
 
-  KB_SCROLL_LOCK = 0x46,
+#define KEYBOARD_NUM_LOCK 0x45
 
-  KB_KEYPAD_7 = 0x47,
-  KB_KEYPAD_8 = 0x48,
-  KB_KEYPAD_9 = 0x49,
+#define KEYBOARD_SCROLL_LOCK 0x46
 
-  KB_KEYPAD_MINUS = 0x4a,
+#define KEYBOARD_KEYPAD_7 0x47
+#define KEYBOARD_KEYPAD_8 0x48
+#define KEYBOARD_KEYPAD_9 0x49
 
-  KB_KEYPAD_4 = 0x4b,
-  KB_KEYPAD_5 = 0x4c,
-  KB_KEYPAD_6 = 0x4d,
-  KB_KEYPAD_PLUS = 0x4e,
+#define KEYBOARD_KEYPAD_MINUS 0x4a
 
-  KB_KEYPAD_1 = 0x4f,
-  KB_KEYPAD_2 = 0x50,
-  KB_KEYPAD_3 = 0x51,
+#define KEYBOARD_KEYPAD_4 0x4b
+#define KEYBOARD_KEYPAD_5 0x4c
+#define KEYBOARD_KEYPAD_6 0x4d
+#define KEYBOARD_KEYPAD_PLUS 0x4e
 
-  KB_KEYPAD_0 = 0x52,
-  KB_KEYPAD_DOT = 0x53,
+#define KEYBOARD_KEYPAD_1 0x4f
+#define KEYBOARD_KEYPAD_2 0x50
+#define KEYBOARD_KEYPAD_3 0x51
 
-  KB_F11 = 0x57,
-  KB_F12 = 0x58
-};
+#define KEYBOARD_KEYPAD_0 0x52
+#define KEYBOARD_KEYPAD_DOT 0x53
+
+#define KEYBOARD_F11 0x57
+#define KEYBOARD_F12 0x58
 
 void keyboard_interrupt();
 
--- sys/keyboard.c
+++ sys/keyboard.c
@@ -8,12 +8,12 @@ uint8_t hex_to_char(uint8_t);
 
 uint8_t hex_to_char(uint8_t key) {
 
-  if(key == KB_L_SHIFT || key == KB_R_SHIFT || key == KB_CAPS) {
+  if(key == KEYBOARD_L_SHIFT || key == KEYBOARD_R_SHIFT || key == KEYBOARD_CAPS) {
     shift = 1;
     return 0;
   }
 
-  if(key == KB_L_CONTROL) {
+  if(key == KEYBOARD_L_CONTROL) {
     ctrl = 1;
     t_write_to_screen("CTRL");
     return 0;
@@ -21,11 +21,11 @@ uint8_t hex_to_char(uint8_t key) {
 
   if(ctrl == 1) {
     ctrl = 0;
-    if(key == KB_C) {
+    if(key == KEYBOARD_C) {
       t_write_to_screen("^C");
-    } else if(key == KB_Z) {
+    } else if(key == KEYBOARD_Z) {
       t_write_to_screen("^Z");
-    } else if(key == KB_D) {
+    } else if(key == KEYBOARD_D) {
       t_write_to_screen("^D");
     }
     return 0;
@@ -34,70 +34,70 @@ uint8_t hex_to_char(uint8_t key) {
   //case where shift key is pressed
   if(shift == 1) {
     shift = 0;
-    if(key >= KB_1 && key <= KB_EQUAL_PLUS) {
-      return "!@#$%^&*()_+"[key - KB_1];
+    if(key >= KEYBOARD_1 && key <= KEYBOARD_EQUAL_PLUS) {
+      return "!@#$%^&*()_+"[key - KEYBOARD_1];
     }
 
-    if(key >= KB_Q && key <= KB_BRACES_CLOSE) {
-      return "QWERTYUIOP{}"[key - KB_Q];
+    if(key >= KEYBOARD_Q && key <= KEYBOARD_BRACES_CLOSE) {
+      return "QWERTYUIOP{}"[key - KEYBOARD_Q];
     }
 
-    if(key >= KB_A && key <= KB_INVERTED_COMMA) {
-      return "ASDFGHJKL:\""[key - KB_A];
+    if(key >= KEYBOARD_A && key <= KEYBOARD_INVERTED_COMMA) {
+      return "ASDFGHJKL:\""[key - KEYBOARD_A];
     }
 
-    if(key >= KB_Z && key <= KB_F_SLASH_QUESTION) {
-      return "ZXCVBNM<>?"[key - KB_Z];
+    if(key >= KEYBOARD_Z && key <= KEYBOARD_F_SLASH_QUESTION) {
+      return "ZXCVBNM<>?"[key - KEYBOARD_Z];
     }
 
-    if(key == KB_TILDE) {
+    if(key == KEYBOARD_TILDE) {
       return '~';
     }
 
-    if(key == KB_B_SLASH) {
+    if(key == KEYBOARD_B_SLASH) {
       return '|';
     }
 
     return 0;
   }
 
-  if(key >= KB_1 && key <= KB_EQUAL_PLUS) {
-    return "1234567890-="[key - KB_1];
+  if(key >= KEYBOARD_1 && key <= KEYBOARD_EQUAL_PLUS) {
+    return "1234567890-="[key - KEYBOARD_1];
   }
 
-  if(key >= KB_Q && key <= KB_BRACES_CLOSE) {
-    return "qwertyuiop[]"[key - KB_Q];
+  if(key >= KEYBOARD_Q && key <= KEYBOARD_BRACES_CLOSE) {
+    return "qwertyuiop[]"[key - KEYBOARD_Q];
   }
 
-  if(key >= KB_A && key <= KB_INVERTED_COMMA) {
-    return "asdfghjkl;'"[key - KB_A];
+  if(key >= KEYBOARD_A && key <= KEYBOARD_INVERTED_COMMA) {
+    return "asdfghjkl;'"[key - KEYBOARD_A];
   }
 
-  if(key >= KB_Z && key <= KB_F_SLASH_QUESTION) {
-    return "zxcvbnm,./"[key - KB_Z];
+  if(key >= KEYBOARD_Z && key <= KEYBOARD_F_SLASH_QUESTION) {
+    return "zxcvbnm,./"[key - KEYBOARD_Z];
   }
 
-  if(key >= KB_KEYPAD_7 && key <= KB_KEYPAD_DOT) {
-    return "789-456+1230."[key - KB_KEYPAD_7];
+  if(key >= KEYBOARD_KEYPAD_7 && key <= KEYBOARD_KEYPAD_DOT) {
+    return "789-456+1230."[key - KEYBOARD_KEYPAD_7];
   }
 
-  if(key == KB_TILDE) {
+  if(key == KEYBOARD_TILDE) {
     return '`';
   }
 
-  if(key == KB_B_SLASH) {
+  if(key == KEYBOARD_B_SLASH) {
     return '\\';
   }
 
-  if(key == KB_SPACE) {
+  if(key == KEYBOARD_SPACE) {
     return ' ';
   }
 
-  if(key == KB_ENTER) {
+  if(key == KEYBOARD_ENTER) {
     t_add_to_buffer('\n');
   }
 
-  if(key == KB_BACKSPACE) {
+  if(key == KEYBOARD_BACKSPACE) {
     t_backspace();
   }
 
